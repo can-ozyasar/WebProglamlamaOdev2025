225003ef751a249ffa3e0e16c94044fa3fcf547b
Ozyasar <canozyasar35@gmail.com>
2025-11-16 18:56:43 +0300

    şablon dosya formatı oluşturuldu.

---
+++ SporSalonu/OZ_SporSalonu/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace OZ_SporSalonu.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
